4ca11f1fa6267ad770df464d14d28e6719dcbb0c
Shrijan Bajracharya <shrijan.buzz.5@gmail.com>
2020-07-02 20:20:15 +0545

    revise 14

--- Assignment II/14.py
+++ Assignment II/14.py
@@ -5,29 +5,11 @@
 # [{'name': 'George', 'address': '4312 Abbey Road', 'age': 22}, {'name':
 # 'John', 'address': '54 Love Ave', 'age': 21}]
 
-file_list = []
-key = []
-values = []
-d = {}
-result = []
+import csv
 
-
-def create_dic():
-    file = open('file.csv', 'r')
-    for line in file:
-        file_list.append(line.split(','))
-    file.close()
-    for i in range(0, 3):
-        key.append(file_list[0][i])
-
-    for i in range(1, len(file_list)):
-        values.append(file_list[i])
-
-    for j in range(0, 2):
-        for i in range(0, 3):
-            d[key[i]] = values[j][i]
-        result.append(d)
-    return result
-
-
-print(create_dic())
+with open('file.csv', mode='r') as infile:
+    reader = csv.DictReader(infile)
+    list_of_dict = []
+    for row in reader:
+        list_of_dict.append(dict(row))
+    print(list_of_dict)
