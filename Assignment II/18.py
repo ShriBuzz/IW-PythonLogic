81854ad3aadcc2a55d5ea9b59479e2fac17ed1ce
Shrijan Bajracharya <shrijan.buzz.5@gmail.com>
2020-07-02 20:18:24 +0545

    Add 18, 20

--- Assignment II/18.py
+++ Assignment II/18.py
@@ -4,3 +4,17 @@
 # module. Serialize a dictionary mapping 'name' to your name and 'age'
 # to your age, to a JSON string. Deserialize the JSON back into
 # Python.
+
+import json
+# help(json)
+
+
+# Serialize a dictionary
+dict_info = {'name': 'Buzz', 'age': 22}
+with open("data_file.json", "w") as write_file:
+    json.dump(dict_info, write_file)
+
+# Deserialize the JSON back into Python
+with open("data_file.json", "r") as read_file:
+    data = json.load(read_file)
+print(data)
